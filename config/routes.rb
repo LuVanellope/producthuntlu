c4214b3c9fad1ed1999f1264f3c3202c299aeae8
Lu <luacosta.fc@gmail.com>
2018-10-02 18:16:31 -0500

    Implementa la funcionalidad de salir

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 
     get 'login', to:'sessions#new'
     post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     resources :users, only:[:new, :create]
     resources :products
 
